ef008d8d213f51fbe4da2a6c30574ae9919c25b3
Gabriel Falcao <gabriel@nacaolivre.org>
2013-02-19 18:16:57 -0500

    creating separated files for the current backends

---
+++ excellent/backends/xl_backend.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from xlwt import Font, XFStyle, Borders, Alignment, Workbook
+
+
+class XL(object):
+    def __init__(self, delimiter=','):
+        self.workbook = Workbook()
+
+    def get_header_style(self):
+        style = XFStyle()
+        style.alignment = Alignment()
+        style.alignment.horz = Alignment.HORZ_RIGHT
+        style.borders = Borders()
+        style.font = Font()
+        style.font.bold = True
+        return style
+
+    def write_row(self, row, values, style=None):
+        for index, value in enumerate(values):
+            row.write(index, value, style or XFStyle())
+
+    def write(self, data, output):
+        sheet = self.workbook.add_sheet('Sheet1')
+        for i, row in enumerate(data):
+            if i is 0:
+                self.write_row(sheet.row(0), row.keys(), self.get_header_style())
+
+            self.write_row(sheet.row(i + 1), row.values())
+
+        self.workbook.save(output)
