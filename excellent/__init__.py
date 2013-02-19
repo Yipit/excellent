022d0572a5318f253e4de481eded455b971b7e05
Gabriel Falcao <gabriel@nacaolivre.org>
2013-02-19 16:45:37 -0500

    more complicated tests for csv writing

--- excellent/__init__.py
+++ excellent/__init__.py
@@ -6,12 +6,13 @@ from StringIO import StringIO
 
 
 class Writer(object):
-    def __init__(self, format):
+    def __init__(self, format, delimiter=','):
         self.format = format
         self.buffer = StringIO()
+        self.delimiter = delimiter
 
     def write(self, data):
-        csv_writer = csv.writer(self.buffer)
+        csv_writer = csv.writer(self.buffer, delimiter=self.delimiter)
         for i, row in enumerate(data):
             if i is 0:
                 csv_writer.writerow(row.keys())
--- tests/functional/test_writer.py
+++ tests/functional/test_writer.py
@@ -17,3 +17,41 @@ def test_write_data_with_header_to_csv():
     # Then the written to data should match our expectation
     (expect(writer.get_value())
      .to.equal("Country,Revenue\r\nArgentina,14500025\r\n"))
+
+
+def test_write_data_with_header_and_multiple_rows_to_csv():
+    "Writer should write data with header and multiple rows correctly in CSV format"
+    # Given a CSV writer
+    writer = Writer(format="csv")
+
+    # When we write some data
+    data = [{"Country": "Argentina", "Revenue": 14500025},
+            {"Country": "Brazil", "Revenue": 145002495}]
+    writer.write(data)
+
+    # Then the written to data should match our expectation
+    expect(writer.get_value()).to.equal("\r\n".join([
+        "Country,Revenue",
+        "Argentina,14500025",
+        "Brazil,145002495",
+        "",
+    ]))
+
+
+def test_writing_with_nondefault_delimiter():
+    "Writer should allow nondefault delimiter"
+    # Given a CSV writer
+    writer = Writer(format="csv", delimiter=";")
+
+    # When we write some data
+    data = [{"Country": "Argentina", "Revenue": 14500025},
+            {"Country": "Brazil", "Revenue": 145002495}]
+    writer.write(data)
+
+    # Then the written to data should match our expectation
+    expect(writer.get_value()).to.equal("\r\n".join([
+        "Country;Revenue",
+        "Argentina;14500025",
+        "Brazil;145002495",
+        "",
+    ]))
