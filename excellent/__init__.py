18281b0732f09c29d82c56c2550cce54bbe03b26
suneel0101 <suneel0101@gmail.com>
2013-02-20 17:44:26 -0500

    adding version to excellent

--- excellent/__init__.py
+++ excellent/__init__.py
@@ -15,6 +15,7 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+version = '0.0.1'
 
 from collections import OrderedDict
 from StringIO import StringIO
